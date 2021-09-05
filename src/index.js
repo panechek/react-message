ec847f03e05df14953ea8dcb6c79315ed1fd684e
panechek <panechek@inbox.ru>
2021-09-05 13:10:12 +0300

    redux-start

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
 import App from './App';
-import {
-  ThemeProvider
-} from '@material-ui/core/styles';
-import { theme } from "./theme";
+import {ThemeProvider} from '@material-ui/core/styles';
+import {theme} from "./theme";
+import store from "./store";
 
 ReactDOM.render(< React.StrictMode >
   <ThemeProvider theme={theme}>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </ThemeProvider>
 
 </React.StrictMode>,
